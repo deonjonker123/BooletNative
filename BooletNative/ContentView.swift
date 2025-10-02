50f149b8a4b897c2240c78caa4b538188dca5a8e
Deon Jonker <deon.jonker@hotmail.com>
2025-10-02 15:24:53 +0200

    Initial Commit

---
+++ BooletNative/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BooletNative
+//
+//  Created by Deon Jonker on 2025/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
